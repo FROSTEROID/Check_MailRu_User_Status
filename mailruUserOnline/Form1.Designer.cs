7ac0506ff108df069dd3e6077b66493874c31f71
FROSTEROID <frosteroidous@gmail.com>
2015-04-28 00:32:10 +0500

    Initialize me!

---
+++ mailruUserOnline/Form1.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace mailruUserOnline
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.tb_addr = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pb_status = new System.Windows.Forms.PictureBox();
+            this.b_check = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pb_status)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // tb_addr
+            // 
+            this.tb_addr.Location = new System.Drawing.Point(12, 65);
+            this.tb_addr.Name = "tb_addr";
+            this.tb_addr.Size = new System.Drawing.Size(100, 20);
+            this.tb_addr.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 49);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(28, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "addr";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(144, 49);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(35, 13);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "status";
+            // 
+            // pb_status
+            // 
+            this.pb_status.Location = new System.Drawing.Point(147, 65);
+            this.pb_status.Name = "pb_status";
+            this.pb_status.Size = new System.Drawing.Size(62, 55);
+            this.pb_status.TabIndex = 3;
+            this.pb_status.TabStop = false;
+            // 
+            // b_check
+            // 
+            this.b_check.Location = new System.Drawing.Point(28, 91);
+            this.b_check.Name = "b_check";
+            this.b_check.Size = new System.Drawing.Size(58, 39);
+            this.b_check.TabIndex = 4;
+            this.b_check.Text = "Check!";
+            this.b_check.UseVisualStyleBackColor = true;
+            this.b_check.Click += new System.EventHandler(this.b_check_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(284, 261);
+            this.Controls.Add(this.b_check);
+            this.Controls.Add(this.pb_status);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tb_addr);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pb_status)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox tb_addr;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pb_status;
+        private System.Windows.Forms.Button b_check;
+    }
+}
+
---
+++ mailruUserOnline/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using System.Net;
+
+namespace mailruUserOnline
+{
+    public partial class Form1 : Form
+    {
+
+        public const string STATUS_URL = "http://status.mail.ru/?";
+        WebClient _client;
+
+        public Form1(){
+			_client = new WebClient();
+			_client.DownloadDataCompleted += Client_DownloadDataCompleted;
+            InitializeComponent();
+        }
+
+		void Client_DownloadDataCompleted(object sender, DownloadDataCompletedEventArgs e){
+			MessageBox.Show(e.Result.Length.ToString());
+			var img = Image.FromStream(new System.IO.MemoryStream(e.Result));
+			pb_status.Image = img;
+		}
+
+        private void b_check_Click(object sender, EventArgs e){
+            string addr = STATUS_URL+tb_addr.Text;
+			_client.DownloadDataAsync(new System.Uri(addr));
+        }
+    }
+}
